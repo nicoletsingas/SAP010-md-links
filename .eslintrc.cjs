b4786c9e4af3d9116d1e0829dcd0547348e59266
Nicole Tsingas <nicolecass27@gmail.com>
2023-07-03 10:16:57 -0300

    Resolvido um bug do eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,17 +5,6 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
